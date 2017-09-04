2422c34581650f3f043858d63ba2fc0924343704
王晗 <caesar.w.h.1996@gmail.com>
2017-09-04 21:21:21 +0800

    integration kafka

--- computing-core/src/main/scala/edu/cs/scu/analysis/Main.scala
+++ computing-core/src/main/scala/edu/cs/scu/analysis/Main.scala
@@ -2,9 +2,9 @@ package edu.cs.scu.analysis
 
 import java.util.concurrent.{ExecutorService, Executors}
 
-import edu.cs.scu.common.constants.AnalysisConstants
 import edu.cs.scu.analysis.offlineanalysis.{GetStayTime, OfflineMain}
 import edu.cs.scu.analysis.realtimeanalysis.RealTimeAnalysis
+import edu.cs.scu.common.constants.AnalysisConstants
 import edu.cs.scu.scalautils.InitUtil
 import org.apache.spark.streaming.StreamingContext
 
@@ -24,21 +24,27 @@ object Main {
     val spark = InitUtil.initSparkSession()
     // 流环境
     val streamingContext: StreamingContext = InitUtil.getStreamingContext(spark.sparkContext)
-//        val kafkaData = InitUtil.getDStreamFromKafka(streamingContext).map(_._2)
-//        kafkaData.print()
+
     val threadPool: ExecutorService = Executors.newFixedThreadPool(AnalysisConstants.THREADS_NUM)
-    try{
+    try {
       threadPool.execute(new GetStayTime)
       threadPool.execute(new OfflineMain)
-    }finally {
+    } finally {
       threadPool.shutdown()
     }
-    // 获取原始数据
-    val originData = InitUtil.getDStream(streamingContext)
+
+    val kafkaData = InitUtil.getDStreamFromKafka(streamingContext).map(_._2)
     // 如果读入的数据不为空
-    if (originData != null) {
-      RealTimeAnalysis.analysis(spark, streamingContext, originData)
+    if (kafkaData != null) {
+      RealTimeAnalysis.analysis(spark, streamingContext, kafkaData)
     }
+
+    //    // 获取原始数据
+    //    val originData = InitUtil.getDStream(streamingContext)
+    //    // 如果读入的数据不为空
+    //    if (originData != null) {
+    //      RealTimeAnalysis.analysis(spark, streamingContext, originData)
+    //    }
     // 启动流环境
     streamingContext.start()
     streamingContext.awaitTermination()
