06f7747c5d970adee8c5bc8506c1c87cd482ac89
Maicius <maicius@outlook.com>
2019-10-03 13:53:57 +0800

    修改模拟发包的脚本

--- py-script/SimulatedSendPackets.py
+++ py-script/SimulatedSendPackets.py
@@ -3,7 +3,7 @@ import random
 import threading
 from time import ctime,sleep
 import json
-import urllib2
+import requests
 import time
 def random_mac():
     macList = []
@@ -29,17 +29,8 @@ def random_id():
 
 probeList = []
 
-
-# def randomProbe(times):
-#
-#     for i in range(1000):
-#         probe = {"id": random_id(), "mmac": random_mac(), "rate": 3, "wssid": "test", "wmac": random_mac(), "time": }
-#         probes = json.dumps(probe)
-#         probeList.append(probes)
-
-
-def random_json():
-
+# 模拟探针发包
+def send_random_json():
     headers = {'Content-Type': 'application/json'}
     probe = {"id": ''+random_id(), "mmac": random_mac(), "rate": "3", "wssid": "test", "wmac": random_mac(), "time": time.strftime('%a %b %e %H:%M:%S %Y', time.localtime(time.time()))}
     mac_data ={"mac": random_mac(), "rssi": random_rssi(), "range": random_range()}
@@ -50,33 +41,31 @@ def random_json():
     probe['data'] = mac_DataMul
 
     probe = json.dumps(probe)
+    print(probe)
+    request = requests.post(url='http://localhost:8080/upload.action', headers=headers, data=probe)
+    response = request.content.decode("utf-8")
+    print("response:", response)
 
-    print probe
-    request = urllib2.Request(url='http://localhost:8080/upload.action', headers=headers, data=probe)
-    response = urllib2.urlopen(request)
-    print "response:" + response
+# 模拟多线程发包
+def send_rand_json_with_multi_thread():
+    for i in range(10):
+        probe = {"id": i, "mmac": random_mac(), "rate": 3, "wssid": "test", "wmac": random_mac()}
+        probes = json.dumps(probe)
+        probeList.append(probes)
 
+    for i in range(10):
+        t = threading.Thread(target=send_random_json)
+        threads.append(t)
+
+    for i in range(10):
+        threads[i].setDaemon(True)
+        threads[i].start()
 
 if __name__ == '__main__':
     threads = []
     probeList = []
     index=0
-    # for i in range(10):
-    #     probe = {"id": i, "mmac": random_mac(), "rate": 3, "wssid": "test", "wmac": random_mac()}
-    #     probes = json.dumps(probe)
-    #     probeList.append(probes)
-    # for i in range(10):
-    #     t = threading.Thread(target=random_json)
-    #     threads.append(t)
-    # for i in range(10):
-    #     threads[i].setDaemon(True)
-    #     threads[i].start()
     for i in range(1000):
-        random_json()
+        send_random_json()
         sleep(1)
-    print "all over %s" %ctime()
-# print random_mac()
-# print random_rssi()
-# print random_range()
-
-# print random_json()
+    print("all over %s" %ctime())
